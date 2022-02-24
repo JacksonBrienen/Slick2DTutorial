3f0ca17d27cebe259fa730fa6be05b69a8386df4
Jackson Brienen <jacksonbrienenjavadevelopment@gmail.com>
2022-02-24 06:30:42 -0800

    Example Project and ReadMe walkthrough
    Read Me File now includes a better description along with a small Tutorial on how to set up the first Slick2D project.
    The Example project shown the in Tutorial is uploaded in case of issues following the tutorial.

---
+++ Example/src/com/github/jacksonbrienen/example/Game.java
@@ -0,0 +1,46 @@
+package com.github.jacksonbrienen.example;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame{
+	
+	/*
+	 * This is the default constructor for a BasicGame, the parameter title will be the title of the window
+	 */
+	public Game(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	/*
+	 * This method will be called before the render or update methods are run, 
+	 * this method is usually run when the AppGameContainer's start method is called
+	 */
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	/*
+	 * This method gets called every frame used to update the graphics on the screen
+	 */
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	/*
+	 * This method gets called every frame used to update the positions or mathmatics needed for the objects on the screen
+	 */
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
